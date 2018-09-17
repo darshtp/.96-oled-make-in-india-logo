d22904c341eda72fc1fd8d0a9316cdc64eab41ed
darshtp <42264279+darshtp@users.noreply.github.com>
2018-09-18 00:20:09 +0530

    Add files via upload

---
+++ _96-oled-make-in-india-logo.ino
@@ -0,0 +1,74 @@
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+
+// Include custom images
+#include "images.h"
+
+// Initialize the OLED display using brzo_i2c
+//    D3 -> SDA
+//    D5 -> SCL
+// SSD1306Brzo display(0x3c, D3, D5);
+// or
+// SH1106Brzo  display(0x3c, D3, D5);
+
+// Initialize the OLED display using Wire library
+SSD1306  display(0x3c, D3, D5);
+
+
+#define DEMO_DURATION 5000
+typedef void (*Demo)(void);
+
+int demoMode = 0;
+int counter = 1;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  // Initialising the UI will init the display too.
+  display.init();
+
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+
+}
+
+void drawFontFaceDemo() {
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(0, 0, "Make");
+    display.setFont(ArialMT_Plain_16);
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.drawString(64, 22, "in");
+    display.setFont(ArialMT_Plain_24);
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.drawString(128, 33, "India");
+}
+
+void drawImageDemo() {
+    display.drawXbm(34, 14, oled_width, oled_height, oled_bits);
+}
+
+Demo demos[] = {drawFontFaceDemo, drawImageDemo};
+int demoLength = (sizeof(demos) / sizeof(Demo));
+long timeSinceLastModeSwitch = 0;
+
+void loop() {
+  // clear the display
+  display.clear();
+  // draw the current demo method
+  demos[demoMode]();
+
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.drawString(10, 128, String(millis()));
+  // write the buffer to the display
+  display.display();
+
+  if (millis() - timeSinceLastModeSwitch > DEMO_DURATION) {
+    demoMode = (demoMode + 1)  % demoLength;
+    timeSinceLastModeSwitch = millis();
+  }
+  counter++;
+  delay(10);
+}
